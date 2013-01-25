bf0fc0e0ad5f6faf842c0848f76d762fa56bf4cd
Mike Allanson <michael.allanson@gmail.com>
2013-01-25 14:50:27 +0000

    Remove leftover var.

--- tasks/ghost.js
+++ tasks/ghost.js
@@ -18,8 +18,7 @@ module.exports = function (grunt) {
         // Get spawn function for the CasperJS process creation
         spawn = require('child_process').spawn,
         // Create array that will contain all the parameters for CasperJS
-        command = [],
-        len;
+        command = [];
 
     // Get CasperJS test options and add them to command array
     if (options.xunit) {
